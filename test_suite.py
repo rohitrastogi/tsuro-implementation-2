373389d72376d96bb767919a6072ac8cadfce250
Upasna Madhok <upasnamadhok2018@u.northwestern.edu>
2018-04-16 21:35:14 -0500

    tests for getNextBoardPosition

--- test_suite.py
+++ test_suite.py
@@ -6,3 +6,10 @@ import administrator
 def test_getCoordinates():
     assert administrator.get_coordinates((3,2)) == [(10,9),(11,9),(12,8),(12,7),(11,6),(10,6),(9,7),(9,8)]
     assert administrator.get_coordinates((1,4)) == [(4,15),(5,15),(6,14),(6,13),(5,12),(4,12),(3,13),(3,14)]
+
+def test_getNextBoardPosition():
+    assert administrator.get_next_board_position((9,7), (3,2)) == (2,2)
+    assert administrator.get_next_board_position((1,0), (0,-1)) == (0,0)
+    assert administrator.get_next_board_position((18,4), (6,1)) == (5,1)
+    assert administrator.get_next_board_position((0,8), (-1,2)) == (0,2)
+    assert administrator.get_next_board_position((11,18), (3,6)) == (3,5)
