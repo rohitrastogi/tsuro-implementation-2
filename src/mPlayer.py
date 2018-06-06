19bd7c97fd5a9aaa6bc6ea9fed16409b175d0804
Upasna Madhok <upasnamadhok2018@u.northwestern.edu>
2018-06-05 22:54:47 -0500

    added legal_play back to mplayer because of tournament

--- src/mPlayer.py
+++ src/mPlayer.py
@@ -71,6 +71,34 @@ class MPlayer(implements(IPlayer)):
                 return True
         return False
 
+    def legal_play(self, board, tile, tiles):
+        """
+        Returns whether the tile the player wants to play is a legal move or not, considering all the tiles the player has.
+
+        Args:
+            self: instance of player class, the player whose turn it is
+            board: instance of Board class, current state of the board
+            tile: instance of Tile class, the tile the player wants to play
+
+        Returns:
+            boolean. True, if the tile is a legal move. False, otherwise.
+        """
+
+        # First, check if there is another legal move that can be played.
+        another_legal = False
+        for a_tile in tiles:
+            for i in range(constants.NUMBER_OF_ROTATIONS):
+                a_tile.rotate_tile()
+                if self.legal_play_helper(board, a_tile, tiles):
+                    another_legal = True
+
+        # If there is another legal move, just check whether this tile causes elimination or not
+        # If there is not another legal move, then this tile can be played provided it belongs to the player
+        if another_legal:
+            return self.legal_play_helper(board, tile, tiles)
+        else:
+            return self.is_tile_owned(tile, tiles)
+
     def legal_play_helper(self, board, tile, tiles):
         """
         Returns whether the tile the player wants to play is a legal move or not, regardless of other tiles in the player's hand.
